555232e89168e698c84ec651ffd12edc247f6215
Rahul <rahulchunduruu@gmail.com>
2026-03-04 15:01:59 +0530

    Change API key loading to use Streamlit secrets
    Updated API key retrieval method to use Streamlit secrets.

--- crew/tools/config.py
+++ crew/tools/config.py
@@ -6,6 +6,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class config:
-    TAVILY_API_KEY=os.getenv('TAVILY_API_KEY')
-    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-    XAI_API_KEY=os.getenv('XAI_API_KEY')
+    #If you prefer to run this in the terminal, make sure the first 3 lines is active and the next 3 lines is commented out
+    #TAVILY_API_KEY=os.getenv('TAVILY_API_KEY')
+    #OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+    #XAI_API_KEY=os.getenv('XAI_API_KEY')
+    OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+    TAVILY_API_KEY=st.secrets['TAVILY_API_KEY']
+    XAI_API_KEY=st.secrets['XAI_API_KEY']
+
